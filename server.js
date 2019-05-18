ca2c961253689424b660478b4c856fed7a94b2fc
TejjyMK <kantejj.atijohn@gmail.com>
2019-05-18 11:22:53 +0100

    serving static files

--- server.js
+++ server.js
@@ -6,21 +6,24 @@ app.listen(3000,function () {
     console.log('Server is listening on port 3000. Ready to accept requests');
 });
 
-app.get("/", function (req, res) {
+// app.get("/", function (req, res) {
+//
+// res.send("Yay Node Girls");
+// });
+//
+// app.get("/chocolate", function (req,res) {
+//     res.send("Mm chocolate :o");
+// });
+//
+// // ? Challenge
+//
+// app.get("/node", function (req, res) {
+//     res.send("Yo Node");
+// });
+//
+// app.get("/girls", function (req, res) {
+//     res.send("Sup Girls");
+// });
 
-res.send("Yay Node Girls");
-});
-
-app.get("/chocolate", function (req,res) {
-    res.send("Mm chocolate :o");
-});
-
-// ? Challenge
-
-app.get("/node", function (req, res) {
-    res.send("Yo Node");
-});
-
-app.get("/girls", function (req, res) {
-    res.send("Sup Girls");
-});
+// ? serving static files
+app.use(express.static("public"));
